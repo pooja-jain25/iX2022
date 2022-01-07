9e70b8c45d4d55210f67c2421873ee40dc2a5841
Pooja Jain <poojajain@Poojas-MacBook-Pro.local>
2022-01-07 12:37:00 -0600

    added days3-5

---
+++ Day_3/contact-book/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
